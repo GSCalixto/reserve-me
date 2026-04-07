6d22b0292f6464fac922e06d9136e026772848af
THEVOL <gabriel_calixto.gsc@hotmail.com>
2026-04-07 15:14:01 -0300

    criando rota de exclusão para restaurantes

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,7 +2,7 @@
 import { httpServer, app } from "./http.js";
 import "./webSocket.js";
 
-import { selecionaRestaurante } from "./config/database.js";
+import { selecionaRestaurante, cadastraRestaurante, deletarRestaurante } from "./config/database.js";
 
 
 //const con = global.connection;
@@ -26,6 +26,34 @@ app.get('/restaurantes', async (req, res) => {
 
   } catch (error) {
     console.error('Erro ao processar a requisição:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
+
+app.post('/cadastrar_restaurante', async (req, res) => { 
+  try {
+    const { name, email_address } = req.body;
+    console.log(req.body);
+    const novoRestaurante = await cadastraRestaurante(name, email_address);
+    res.status(201).json(novoRestaurante);
+  } catch (error) {
+    console.error('Erro ao cadastrar restaurante:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
+
+app.delete('/deletar_restaurante/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const restauranteDeletado = await deletarRestaurante(id);
+    if (restauranteDeletado) {
+      res.json({ message: 'Restaurante deletado com sucesso', restaurante: restauranteDeletado });
+    } else {
+      res.status(404).json({ error: 'Restaurante não encontrado' });
+    };
+    // Lógica para deletar o restaurante com o ID especificado
+  } catch (error) {
+    console.error('Erro ao deletar restaurante:', error);
     res.status(500).json({ error: 'Erro interno do servidor' });
   }
 });
--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -1,3 +1,5 @@
+// Eventualmente terei que modularizar as responsabilidades dessa bosta
+
 import 'dotenv/config';
 import { Pool } from 'pg';
 
@@ -19,19 +21,49 @@ async function connect() {
 
   global.connection = pool;
   return await pool.connect();
-}
+};
 
 connect();
 
 async function selecionaRestaurante() {
-
   const cliente = await connect();
-  const restaurante = await cliente.query("SELECT * FROM restaurante");
-  const res = restaurante.rows;
-  return res;
-}
+  
+  try {
+    const restaurante = await cliente.query("SELECT * FROM restaurante");
+    const res = restaurante.rows;
+    return res;
+  } finally {
+    cliente.release();
+  };
+};
 
-export { selecionaRestaurante, connect };
+async function cadastraRestaurante(nome, email_address) {
+  const cliente = await connect();
+
+  try {
+    const texto = `INSERT INTO restaurante (name, email_address) VALUES ($1, $2) RETURNING *`;
+    const values = [nome, email_address];
+    const result = await cliente.query(texto, values);
+    return result.rows[0];
+  } finally {
+    cliente.release();
+  };
+};
+
+async function deletarRestaurante(id) {
+  const cliente = await connect();
+
+  try {
+    const texto = `DELETE FROM restaurante WHERE id = $1 RETURNING *`;
+    const values = [id];
+    const result = await cliente.query(texto, values);
+    return result.rows[0];
+  } finally {
+    cliente.release();
+  };
+};
+
+export { selecionaRestaurante, cadastraRestaurante, deletarRestaurante, connect };
  
 
 // This code sets up a connection pool to a PostgreSQL database using the 'pg' library.
